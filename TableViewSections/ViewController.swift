e978d6218e21fbda010504679949d805f67bdc38
Marcin Karski <karski80@gmail.com>
2019-02-04 20:23:55 +0000

    Initial commit

--- TableViewSections/ViewController.swift
+++ TableViewSections/ViewController.swift
@@ -1,20 +1,14 @@
-//
-//  ViewController.swift
-//  TableViewSections
-//
-//  Created by Marcin Karski on 04/02/2019.
-//  Copyright © 2019 Marcin Karski. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setup()
     }
-
-
+    
+    private func setup() {
+        view.backgroundColor = .white
+        self.title = "Initial Setup"
+    }
 }
-
